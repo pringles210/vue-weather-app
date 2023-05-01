d7c4435709c969cd4227d369c8114ce175f811b7
pringles210 <palmithor94@gmail.com>
2023-05-01 21:57:18 +0000

    first commit, vue app created and boilerplate cleanup

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <header>
+    <div class="container" >
+      <WeatherCard class="item" msg="New York" />
+      <WeatherCard class="item" msg="Reykjavik" />
+      <WeatherCard class="item" msg="Berlin" />
+      <WeatherCard class="item" msg="New York" />
+      <WeatherCard class="item" msg="Reykjavik" />
+      <WeatherCard class="item" msg="Berlin" />
+    </div>
+  </header>
+
+</template>
+
+<script setup>
+
+import WeatherCard from './components/WeatherCard.vue'
+
+</script>
+
+<style scoped>
+
+.container {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 10px;
+  padding: 10px;
+   text-align: center;
+}
+
+.item {
+  padding: 20px 0;
+  font-size: 30px;
+}
+</style>
